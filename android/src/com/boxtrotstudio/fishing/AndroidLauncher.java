4ba9df564384f2abe76dfa9ec5f9a5f01a77af9c
hypereddie <ecp4224@gmail.com>
2016-10-17 15:21:24 -0400

    init commit

---
+++ android/src/com/boxtrotstudio/fishing/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.boxtrotstudio.fishing;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.boxtrotstudio.fishing.Fishing;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Fishing(), config);
+	}
+}
